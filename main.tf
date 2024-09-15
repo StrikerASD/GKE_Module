0cf50491be894cf192cbdb825a042b455cd63df5
root <admin@example.com>
2024-09-15 20:27:17 +0200

    Commented default node pool

--- main.tf
+++ main.tf
@@ -25,11 +25,11 @@ resource "google_container_cluster" "primary" {
   deletion_protection = var.deletion_protection
   enable_autopilot = var.enable_autopilot
 
-  # We can't create a cluster with no node pool defined, but we want to only use
-  # separately managed node pools. So we create the smallest possible default
-  # node pool and immediately delete it.
-  remove_default_node_pool = var.container_cluster_remove_default_node_pool
-  initial_node_count       = var.container_cluster_initial_node_count
+#   # We can't create a cluster with no node pool defined, but we want to only use
+#   # separately managed node pools. So we create the smallest possible default
+#   # node pool and immediately delete it.
+#   remove_default_node_pool = var.container_cluster_remove_default_node_pool
+#   initial_node_count       = var.container_cluster_initial_node_count
 }
 
 resource "google_container_node_pool" "primary_preemptible_nodes" {
